7a787824687e2730c77e0e3fe7c673228435c5d8
Douglas McCloskey <dmccloskey87@gmail.com>
2018-02-04 16:06:37 +0100

    tests: EMGModel::z_

--- src/tests/class_tests/smartpeak/source/EMGModel_test.cpp
+++ src/tests/class_tests/smartpeak/source/EMGModel_test.cpp
@@ -9,6 +9,15 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(emgmodel)
 
+class EMGModel_test: public EMGModel
+{
+public:
+  double z_(const double& x_I) const
+  {
+    return EMGModel::z_(x_I);
+  }
+};
+
 BOOST_AUTO_TEST_CASE(constructor) 
 {
   EMGModel* ptr = nullptr;
@@ -36,6 +45,21 @@ BOOST_AUTO_TEST_CASE(gettersAndSetters)
   BOOST_CHECK_EQUAL(emg.getTau(), 2.0);
   BOOST_CHECK_EQUAL(emg.getMu(), 3.0);
   BOOST_CHECK_EQUAL(emg.getSigma(), 4.0);
+}
+
+BOOST_AUTO_TEST_CASE(z_) 
+{
+  EMGModel_test emg;
+  emg.setH(1.0);
+  emg.setTau(0.1);
+  emg.setMu(0.0);
+  emg.setSigma(1.0);
+
+  BOOST_CHECK_CLOSE(emg.z_(-100), 77.781745930520216, 1e-6);
+  BOOST_CHECK_CLOSE(emg.z_(-10), 14.142135623730949, 1e-6);
+  BOOST_CHECK_CLOSE(emg.z_(0), 7.0710678118654746, 1e-6);
+  BOOST_CHECK_CLOSE(emg.z_(10), 0.0, 1e-6);
+  BOOST_CHECK_CLOSE(emg.z_(100), -63.639610306789272, 1e-6);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
