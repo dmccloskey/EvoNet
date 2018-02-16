4e1dff374472e5c15e1ab85dd227e0ab0c672706
Douglas McCloskey <dmccloskey87@gmail.com>
2018-02-16 23:36:27 +0100

    feat: ActivationFunction and LossFunction base classes

---
+++ src/smartpeak/include/SmartPeak/ml/ActivationFunction.h
@@ -0,0 +1,40 @@
+/**TODO:  Add copyright*/
+
+#ifndef SMARTPEAK_ACTIVATIONFUNCTION_H
+#define SMARTPEAK_ACTIVATIONFUNCTION_H
+
+#include <vector>
+
+namespace SmartPeak
+{
+  /**
+    @brief Activation function for a node in a neural network.
+  */
+  class ActivationFunction
+  {
+public:
+    ActivationFunction(); ///< Default constructor    
+    ~ActivationFunction(); ///< Default destructor
+ 
+    /**
+      @brief A function
+
+      @param[in] x_I Input value
+
+      @returns y_O Output value
+    */ 
+    virtual double fx(const double& x_I) const = 0;
+ 
+    /**
+      @brief The first derivative of a function
+
+      @param[in] x_I Input value
+
+      @returns y_O Output value
+    */ 
+    virtual double dfx(const double& x_I) const = 0;
+
+  };
+}
+
+#endif //SMARTPEAK_ACTIVATIONFUNCTION_H
---
+++ src/smartpeak/include/SmartPeak/ml/LossFunction.h
@@ -0,0 +1,32 @@
+/**TODO:  Add copyright*/
+
+#ifndef SMARTPEAK_LOSSFUNCTION_H
+#define SMARTPEAK_LOSSFUNCTION_H
+
+#include <vector>
+
+namespace SmartPeak
+{
+  /**
+    @brief Loss function for a neural network.
+  */
+  class LossFunction
+  {
+public:
+    LossFunction(); ///< Default constructor    
+    ~LossFunction(); ///< Default destructor
+ 
+    /**
+      @brief The loss function
+
+      @param[in] y_pred Predicted value
+      @param[in] y_true True value
+
+      @returns y_O Output value
+    */ 
+    virtual double error(const double& y_pred, const double& y_true) const = 0;
+
+  };
+}
+
+#endif //SMARTPEAK_LOSSFUNCTION_H
