5918ff9cba7dbb9d5aa8915e5f34e65edac60906
Douglas McCloskey <dmccloskey87@gmail.com>
2018-02-20 16:48:40 +0100

    fix: need to implement functors!

--- src/tests/class_tests/smartpeak/source/ReLU_test.cpp
+++ src/tests/class_tests/smartpeak/source/ReLU_test.cpp
@@ -4,10 +4,22 @@
 #include <boost/test/unit_test.hpp>
 #include <SmartPeak/ml/ReLU.h>
 
+#include <unsupported/Eigen/CXX11/Tensor>
+#include <iostream>
+
 using namespace SmartPeak;
 using namespace std;
 
 BOOST_AUTO_TEST_SUITE(relu)
+
+template<typename Scalar>
+class add{
+  public:
+    add(const Scalar& a) : a_(a){}
+    const Scalar operator()(const Scalar& x) const { return x + a_; }
+  private:
+    Scalar a_;
+};
 
 BOOST_AUTO_TEST_CASE(constructor) 
 {
@@ -33,6 +45,13 @@ BOOST_AUTO_TEST_CASE(fx)
   BOOST_CHECK_CLOSE(relu.fx(10.0), 10.0, 1e-6);
   BOOST_CHECK_CLOSE(relu.fx(-1.0), 0.0, 1e-6);
   BOOST_CHECK_CLOSE(relu.fx(-10.0), 0.0, 1e-6);
+
+  Eigen::Tensor<double, 2> a(2.0, 2.0);
+  a.setConstant(1.0f);
+  // Eigen::Tensor<double, 2> b = a.unaryExpr(&dub);
+  Eigen::Tensor<double, 2> b = a.unaryExpr(add<double>(2));
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
 }
 
 BOOST_AUTO_TEST_CASE(dfx) 
