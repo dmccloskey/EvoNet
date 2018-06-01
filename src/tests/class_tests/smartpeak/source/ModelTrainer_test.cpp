aebe7241cf61077dd4f2317edf79e089d08b5973
Douglas McCloskey <dmccloskey87@gmail.com>
2018-06-01 12:56:41 +0200

    tests: added test for DCGToy in ModelTrainer_test

--- src/tests/class_tests/smartpeak/source/ModelTrainer_test.cpp
+++ src/tests/class_tests/smartpeak/source/ModelTrainer_test.cpp
@@ -157,9 +157,7 @@ BOOST_AUTO_TEST_CASE(DAGToy)
   public:
     Model makeModel()
     {
-      /**
-       * Directed Acyclic Graph Toy Network Model
-      */
+      // CopyNPasted from Model_DAG_Test
       Node i1, i2, h1, h2, o1, o2, b1, b2;
       Link l1, l2, l3, l4, lb1, lb2, l5, l6, l7, l8, lb3, lb4;
       Weight w1, w2, w3, w4, wb1, wb2, w5, w6, w7, w8, wb3, wb4;
@@ -336,7 +334,171 @@ BOOST_AUTO_TEST_CASE(DAGToy)
 
 BOOST_AUTO_TEST_CASE(DCGToy) 
 {
-  // [TODO: add tests using DCG toy network]
+
+  // Define the makeModel and trainModel scripts
+  class DCGToyModelTrainer: public ModelTrainer
+  {
+  public:
+    Model makeModel()
+    {
+      // CopyNPasted from Model_DCG_Test
+      Node i1, h1, o1, b1, b2;
+      Link l1, l2, l3, lb1, lb2;
+      Weight w1, w2, w3, wb1, wb2;
+      Model model2;
+      // Toy network: 1 hidden layer, fully connected, DCG
+      i1 = Node("0", NodeType::input, NodeStatus::activated, NodeActivation::Linear);
+      h1 = Node("1", NodeType::hidden, NodeStatus::deactivated, NodeActivation::ELU);
+      o1 = Node("2", NodeType::output, NodeStatus::deactivated, NodeActivation::ELU);
+      b1 = Node("3", NodeType::bias, NodeStatus::activated, NodeActivation::Linear);
+      b2 = Node("4", NodeType::bias, NodeStatus::activated, NodeActivation::Linear);
+      // weights  
+      std::shared_ptr<WeightInitOp> weight_init;
+      std::shared_ptr<SolverOp> solver;
+      // weight_init.reset(new RandWeightInitOp(1.0)); // No random init for testing
+      weight_init.reset(new RandWeightInitOp(1.0));
+      solver.reset(new AdamOp(0.01, 0.9, 0.999, 1e-8));
+      w1 = Weight("0", weight_init, solver);
+      weight_init.reset(new RandWeightInitOp(1.0));
+      solver.reset(new AdamOp(0.01, 0.9, 0.999, 1e-8));
+      w2 = Weight("1", weight_init, solver);
+      weight_init.reset(new RandWeightInitOp(1.0));
+      solver.reset(new AdamOp(0.01, 0.9, 0.999, 1e-8));
+      w3 = Weight("2", weight_init, solver);
+      weight_init.reset(new ConstWeightInitOp(1.0));
+      solver.reset(new AdamOp(0.01, 0.9, 0.999, 1e-8));
+      wb1 = Weight("3", weight_init, solver);
+      weight_init.reset(new ConstWeightInitOp(1.0));
+      solver.reset(new AdamOp(0.01, 0.9, 0.999, 1e-8));
+      wb2 = Weight("4", weight_init, solver);
+      weight_init.reset();
+      solver.reset();
+      // links
+      l1 = Link("0", "0", "1", "0");
+      l2 = Link("1", "1", "2", "1");
+      l3 = Link("2", "2", "1", "2");
+      lb1 = Link("3", "3", "1", "3");
+      lb2 = Link("4", "4", "2", "4");
+      model2.setId(2);
+      model2.setLossFunction(ModelLossFunction::MSE);
+      model2.addNodes({i1, h1, o1, b1, b2});
+      model2.addWeights({w1, w2, w3, wb1, wb2});
+      model2.addLinks({l1, l2, l3, lb1, lb2});
+      return model2;
+    }
+
+    void trainModel(Model& model,
+      const Eigen::Tensor<float, 4>& input,
+      const Eigen::Tensor<float, 3>& output,
+      const Eigen::Tensor<float, 3>& time_steps,
+      const std::vector<std::string>& input_nodes,
+      const std::vector<std::string>& output_nodes)
+    {
+      printf("Training the model\n");
+
+      // Check input and output data
+      if (!checkInputData(getNEpochs(), input, getBatchSize(), getMemorySize(), input_nodes))
+      {
+        return;
+      }
+      if (!checkOutputData(getNEpochs(), output, getBatchSize(), output_nodes))
+      {
+        return;
+      }
+      printf("Data checks passed\n");
+      
+      // Initialize the model
+      model.initNodes(getBatchSize(), getMemorySize());
+      model.initWeights();
+      printf("Initialized the model\n");
+
+      for (int iter = 0; iter < getNEpochs(); ++iter) // use n_epochs here
+      {
+        printf("Training epoch: %d\t", iter);
+
+        // forward propogate
+        model.FPTT(getMemorySize(), input.chip(iter, 3), input_nodes, time_steps.chip(iter, 2)); 
+
+        // calculate the model error and node output error
+        model.calculateError(output.chip(iter, 2), output_nodes);
+        std::cout<<"Model error: "<<model.getError().sum()<<std::endl;
+
+        // back propogate
+        model.TBPTT(getMemorySize()-1);
+
+        // update the weights
+        model.updateWeights(getMemorySize());   
+
+        // reinitialize the model
+        model.reInitializeNodeStatuses();
+        model.initNodes(getBatchSize(), getMemorySize());
+      }
+    }
+
+    void validateModel(Model& model,
+      const Eigen::Tensor<float, 4>& input,
+      const Eigen::Tensor<float, 3>& output,
+      const Eigen::Tensor<float, 3>& time_steps,
+      const std::vector<std::string>& input_nodes,
+      const std::vector<std::string>& output_nodes){}
+  };
+
+  DCGToyModelTrainer trainer;
+
+  // Test parameters
+  trainer.setBatchSize(5);
+  trainer.setMemorySize(8);
+  trainer.setNEpochs(100);
+  const std::vector<std::string> input_nodes = {"0", "3", "4"}; // true inputs + biases
+  const std::vector<std::string> output_nodes = {"2"};
+
+  // Make the input data
+  Eigen::Tensor<float, 4> input_data(trainer.getBatchSize(), trainer.getMemorySize(), input_nodes.size(), trainer.getNEpochs());
+  Eigen::Tensor<float, 3> input_tmp(trainer.getBatchSize(), trainer.getMemorySize(), input_nodes.size()); 
+  input_tmp.setValues(
+    {{{1, 0, 0}, {2, 0, 0}, {3, 0, 0}, {4, 0, 0}, {5, 0, 0}, {6, 0, 0}, {7, 0, 0}, {8, 0, 0}},
+    {{2, 0, 0}, {3, 0, 0}, {4, 0, 0}, {5, 0, 0}, {6, 0, 0}, {7, 0, 0}, {8, 0, 0}, {9, 0, 0}},
+    {{3, 0, 0}, {4, 0, 0}, {5, 0, 0}, {6, 0, 0}, {7, 0, 0}, {8, 0, 0}, {9, 0, 0}, {10, 0, 0}},
+    {{4, 0, 0}, {5, 0, 0}, {6, 0, 0}, {7, 0, 0}, {8, 0, 0}, {9, 0, 0}, {10, 0, 0}, {11, 0, 0}},
+    {{5, 0, 0}, {6, 0, 0}, {7, 0, 0}, {8, 0, 0}, {9, 0, 0}, {10, 0, 0}, {11, 0, 0}, {12, 0, 0}}}
+  );
+  for (int batch_iter=0; batch_iter<trainer.getBatchSize(); ++batch_iter)
+    for (int memory_iter=0; memory_iter<trainer.getMemorySize(); ++memory_iter)
+      for (int nodes_iter=0; nodes_iter<input_nodes.size(); ++nodes_iter)
+        for (int epochs_iter=0; epochs_iter<trainer.getNEpochs(); ++epochs_iter)
+          input_data(batch_iter, memory_iter, nodes_iter, epochs_iter) = input_tmp(batch_iter, memory_iter, nodes_iter);
+  
+  // Make the output data
+  Eigen::Tensor<float, 3> output_data(trainer.getBatchSize(), output_nodes.size(), trainer.getNEpochs());
+  Eigen::Tensor<float, 2> output_tmp(trainer.getBatchSize(), output_nodes.size()); 
+  output_tmp.setValues({{2.5}, {3}, {3.5}, {4}, {4.5}});
+  for (int batch_iter=0; batch_iter<trainer.getBatchSize(); ++batch_iter)
+    for (int nodes_iter=0; nodes_iter<output_nodes.size(); ++nodes_iter)
+      for (int epochs_iter=0; epochs_iter<trainer.getNEpochs(); ++epochs_iter)
+        output_data(batch_iter, nodes_iter, epochs_iter) = output_tmp(batch_iter, nodes_iter);
+
+  // Make the simulation time_steps
+  Eigen::Tensor<float, 3> time_steps(trainer.getBatchSize(), trainer.getMemorySize(), trainer.getNEpochs());
+  Eigen::Tensor<float, 2> time_steps_tmp(trainer.getBatchSize(), trainer.getMemorySize()); 
+  time_steps_tmp.setValues({
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1}}
+  );
+  for (int batch_iter=0; batch_iter<trainer.getBatchSize(); ++batch_iter)
+    for (int memory_iter=0; memory_iter<trainer.getMemorySize(); ++memory_iter)
+      for (int epochs_iter=0; epochs_iter<trainer.getNEpochs(); ++epochs_iter)
+        time_steps(batch_iter, memory_iter, epochs_iter) = time_steps_tmp(batch_iter, memory_iter);
+
+  Model model1 = trainer.makeModel();
+
+  trainer.trainModel(model1, input_data, output_data, time_steps,
+    input_nodes, output_nodes);
+
+  const Eigen::Tensor<float, 0> total_error = model1.getError().sum();
+  BOOST_CHECK(total_error(0) < 10.0);  
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/tests/class_tests/smartpeak/source/PopulationTrainer_test.cpp
+++ src/tests/class_tests/smartpeak/source/PopulationTrainer_test.cpp
@@ -14,123 +14,122 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(populationTrainer)
 
-No Tests as PopulationTrainer is virtual
-BOOST_AUTO_TEST_CASE(constructor) 
-{
-  PopulationTrainer* ptr = nullptr;
-  PopulationTrainer* nullPointer = nullptr;
-	ptr = new PopulationTrainer();
-  BOOST_CHECK_NE(ptr, nullPointer);
-}
+// BOOST_AUTO_TEST_CASE(constructor) 
+// {
+//   PopulationTrainer* ptr = nullptr;
+//   PopulationTrainer* nullPointer = nullptr;
+// 	ptr = new PopulationTrainer();
+//   BOOST_CHECK_NE(ptr, nullPointer);
+// }
 
-BOOST_AUTO_TEST_CASE(destructor) 
-{
-  PopulationTrainer* ptr = nullptr;
-	ptr = new PopulationTrainer();
-  delete ptr;
-}
+// BOOST_AUTO_TEST_CASE(destructor) 
+// {
+//   PopulationTrainer* ptr = nullptr;
+// 	ptr = new PopulationTrainer();
+//   delete ptr;
+// }
 
-BOOST_AUTO_TEST_CASE(DELETEAfterTesting) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(DELETEAfterTesting) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // define the model replicator for growth mode
-  ModelReplicator model_replicator;
-  model_replicator.setNNodeAdditions(1);
-  model_replicator.setNLinkAdditions(1);
-  model_replicator.setNNodeDeletions(0);
-  model_replicator.setNLinkDeletions(0);
+//   // define the model replicator for growth mode
+//   ModelReplicator model_replicator;
+//   model_replicator.setNNodeAdditions(1);
+//   model_replicator.setNLinkAdditions(1);
+//   model_replicator.setNNodeDeletions(0);
+//   model_replicator.setNLinkDeletions(0);
 
-  // define the model trainer
-  class ModelTrainerTest: public ModelTrainer
-  {
-  public:
-    Model makeModel(){};
-    void trainModel(Model& model,
-      const Eigen::Tensor<float, 4>& input,
-      const Eigen::Tensor<float, 3>& output,
-      const Eigen::Tensor<float, 3>& time_steps,
-      const std::vector<std::string>& input_nodes,
-      const std::vector<std::string>& output_nodes)
-    {
-      // [TODO: define DCG training regime]
-    }
-    void validateModel(Model& model,
-      const Eigen::Tensor<float, 4>& input,
-      const Eigen::Tensor<float, 3>& output,
-      const Eigen::Tensor<float, 3>& time_steps,
-      const std::vector<std::string>& input_nodes,
-      const std::vector<std::string>& output_nodes)
-    {
-      // [TODO: define DCG validation regime]
-    }
-  };
-  ModelTrainerTest model_trainer;
-  model_trainer.setBatchSize(5);
-  model_trainer.setMemorySize(8);
-  model_trainer.setNEpochs(100);
+//   // define the model trainer
+//   class ModelTrainerTest: public ModelTrainer
+//   {
+//   public:
+//     Model makeModel(){};
+//     void trainModel(Model& model,
+//       const Eigen::Tensor<float, 4>& input,
+//       const Eigen::Tensor<float, 3>& output,
+//       const Eigen::Tensor<float, 3>& time_steps,
+//       const std::vector<std::string>& input_nodes,
+//       const std::vector<std::string>& output_nodes)
+//     {
+//       // [TODO: define DCG training regime]
+//     }
+//     void validateModel(Model& model,
+//       const Eigen::Tensor<float, 4>& input,
+//       const Eigen::Tensor<float, 3>& output,
+//       const Eigen::Tensor<float, 3>& time_steps,
+//       const std::vector<std::string>& input_nodes,
+//       const std::vector<std::string>& output_nodes)
+//     {
+//       // [TODO: define DCG validation regime]
+//     }
+//   };
+//   ModelTrainerTest model_trainer;
+//   model_trainer.setBatchSize(5);
+//   model_trainer.setMemorySize(8);
+//   model_trainer.setNEpochs(100);
 
-  // define the initial population of 10 baseline models
-  std::vector<Model> population; 
-  std::shared_ptr<WeightInitOp> weight_init;
-  std::shared_ptr<SolverOp> solver;
-  for (int i=0; i<10; ++i)
-  {
-    // baseline model
-    weight_init.reset(new ConstWeightInitOp(1.0));
-    solver.reset(new SGDOp(0.01, 0.9));
-    Model model = model_replicator.makeBaselineModel(
-      1, 0, 1,
-      NodeActivation::ReLU, NodeActivation::ReLU,
-      weight_init, solver);
+//   // define the initial population of 10 baseline models
+//   std::vector<Model> population; 
+//   std::shared_ptr<WeightInitOp> weight_init;
+//   std::shared_ptr<SolverOp> solver;
+//   for (int i=0; i<10; ++i)
+//   {
+//     // baseline model
+//     weight_init.reset(new ConstWeightInitOp(1.0));
+//     solver.reset(new SGDOp(0.01, 0.9));
+//     Model model = model_replicator.makeBaselineModel(
+//       1, 0, 1,
+//       NodeActivation::ReLU, NodeActivation::ReLU,
+//       weight_init, solver);
     
-    // modify the models
-    model_replicator.modifyModel(model);
+//     // modify the models
+//     model_replicator.modifyModel(model);
 
-    population.push_back(model);
-  }
+//     population.push_back(model);
+//   }
 
-  // train the population
-  for (int i=0; i<population.size(); ++i)
-  {
+//   // train the population
+//   for (int i=0; i<population.size(); ++i)
+//   {
 
-  }
-}
+//   }
+// }
 
-BOOST_AUTO_TEST_CASE(selectModels) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(selectModels) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // [TODO: add tests]
-}
+//   // [TODO: add tests]
+// }
 
-BOOST_AUTO_TEST_CASE(copyModels) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(copyModels) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // [TODO: add tests]
-}
+//   // [TODO: add tests]
+// }
 
-BOOST_AUTO_TEST_CASE(modifyModels) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(modifyModels) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // [TODO: add tests]
-}
+//   // [TODO: add tests]
+// }
 
-BOOST_AUTO_TEST_CASE(trainModels) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(trainModels) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // [TODO: add tests]
-}
+//   // [TODO: add tests]
+// }
 
-BOOST_AUTO_TEST_CASE(validateModels) 
-{
-  PopulationTrainer population_trainer;
+// BOOST_AUTO_TEST_CASE(validateModels) 
+// {
+//   PopulationTrainer population_trainer;
 
-  // [TODO: add tests]
-}
+//   // [TODO: add tests]
+// }
 
 
 BOOST_AUTO_TEST_SUITE_END()
