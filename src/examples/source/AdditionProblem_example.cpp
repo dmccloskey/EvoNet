426e1b2ee747f057c626b1934bd47e0b91b45381
Douglas McCloskey <dmccloskey87@gmail.com>
2018-10-20 15:23:40 +0200

    example: change data simulator params for add prob

--- src/examples/source/AdditionProblem_example.cpp
+++ src/examples/source/AdditionProblem_example.cpp
@@ -800,8 +800,8 @@ int main(int argc, char** argv)
 	ModelTrainerExt<float> model_trainer;
 	model_trainer.setBatchSize(1);
 	model_trainer.setMemorySize(data_simulator.sequence_length_);
-	model_trainer.setNEpochsTraining(5);
-	model_trainer.setNEpochsValidation(5);
+	model_trainer.setNEpochsTraining(1000);
+	model_trainer.setNEpochsValidation(25);
 	model_trainer.setVerbosityLevel(1);
 	model_trainer.setNThreads(2);
 	model_trainer.setLogging(true, false);
