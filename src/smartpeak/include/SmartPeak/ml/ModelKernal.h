15bfa23db794c9fc336895d3885a9194d43c77af
dmccloskey <dmccloskey87@gmail.com>
2018-12-06 19:13:39 +0100

    feat: added ability to add on additional conv filters without an intermediate pooling step

--- src/smartpeak/include/SmartPeak/ml/ModelKernal.h
+++ src/smartpeak/include/SmartPeak/ml/ModelKernal.h
@@ -176,11 +176,11 @@ namespace SmartPeak
 			Eigen::TensorMap<Eigen::Tensor<TensorT, 5>> shared_weight_tensor(shared_weights, 1, 1, source_layer_size, sink_layer_size, n_shared_layers);
 			// Step 1: multiply the weight tensor by the shared weight tensor mask; sum all shared weights
 			auto weight_error_sum = (weight_error_tensor.broadcast(Eigen::array<int, 5>({ 1,1,1,1,n_shared_layers })) * shared_weight_tensor
-				).sum(Eigen::array<int, 2>({ 2, 3 })).broadcast(Eigen::array<int, 3>({ source_layer_size, sink_layer_size, 1 }));  // dims 3
+				).sum(Eigen::array<int, 2>({ 2, 3 })).eval().broadcast(Eigen::array<int, 3>({ source_layer_size, sink_layer_size, 1 }));  // dims 3
 			// Step 2: multiply the weight error sum tensor by the shared weight tensor mask and subtract out the error tensor
 			auto weight_error_diff = (weight_error_sum * shared_weight_tensor.chip(0, 1).chip(0, 0) -
 				weight_error_tensor.chip(0, 1).chip(0, 0).broadcast(Eigen::array<int, 3>({ 1,1,n_shared_layers })) * shared_weight_tensor.chip(0, 1).chip(0, 0)
-				).sum(Eigen::array<int, 1>({ 2 })); //dims 2
+				).eval().sum(Eigen::array<int, 1>({ 2 })); //dims 2
 			// Step 3: add the weight_error_diff
 			weight_error_tensor.chip(0, 4).chip(0, 1).chip(0,0).device(device) += weight_error_diff;
 		}
