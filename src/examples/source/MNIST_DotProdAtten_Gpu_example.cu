e6499a3722f6741593cfd87ed6f757e225bb77d6
dmccloskey <dmccloskey87@gmail.com>
2018-12-08 17:05:25 +0100

    fix: bug in examples file naming

--- src/examples/source/MNIST_DotProdAtten_Gpu_example.cu
+++ src/examples/source/MNIST_DotProdAtten_Gpu_example.cu
@@ -19,14 +19,14 @@ using namespace SmartPeak;
 
 /**
  * EXAMPLES using the MNIST data set
- * 
+ *
  * EXAMPLE1:
  * - classification on MNIST using DAG
  * - whole image pixels (linearized) 28x28 normalized to 0 to 1
  * - classifier (1 hot vector from 0 to 9)
  */
 
-// Extended classes
+ // Extended classes
 template<typename TensorT>
 class ModelTrainerExt : public ModelTrainerGpu<TensorT>
 {
@@ -36,11 +36,11 @@ public:
 
 	@param n_heads 8
 	@param n_layers 3
-	@param n_fc 1024 
+	@param n_fc 1024
 	@param add_skip Optional skip connections between layers
 	@param add_norm Optional normalization layer after each convolution
 	*/
-	Model<TensorT> makeCovNet(const int& n_inputs, const int& n_outputs, int n_heads = 8, int n_layers = n_layers, bool add_skip = true, bool add_norm = false) {
+	Model<TensorT> makeMultiHeadDotProdAttention(const int& n_inputs, const int& n_outputs, int n_heads = 8, int n_layers = n_layers, bool add_skip = true, bool add_norm = false) {
 		Model<TensorT> model;
 		model.setId(0);
 		model.setName("DotProdAttent");
@@ -55,10 +55,10 @@ public:
 		for (size_t i = 0; i < n_layers; ++i) {
 			// Add the attention
 			int key_query_values_lengths = n_inputs / n_heads;
-			std::string name_head1 = "Attention0" + std::to_string(i);
+			std::string name_head1 = "Attention" + std::to_string(i);
 			node_names = model_builder.addMultiHeadAttention(model, name_head1, name_head1,
 				node_names_input, node_names_input, node_names_input,
-				n_inputs, "DotProd", key_query_values_lengths, key_query_values_lengths,
+				n_heads, "DotProd", n_inputs, key_query_values_lengths, key_query_values_lengths,
 				std::shared_ptr<ActivationOp<TensorT>>(new LinearOp<TensorT>()),
 				std::shared_ptr<ActivationOp<TensorT>>(new LinearGradOp<TensorT>()),
 				std::shared_ptr<WeightInitOp<TensorT>>(new RandWeightInitOp<TensorT>(n_inputs, 2)),
@@ -97,12 +97,12 @@ public:
 					std::shared_ptr<WeightInitOp<TensorT>>(new RandWeightInitOp<TensorT>(node_names.size(), 2)),
 					std::shared_ptr<SolverOp<TensorT>>(new AdamOp<TensorT>(0.1, 0.9, 0.999, 1e-8)), 0.0, 0.0);
 			}
-			if (add_skip) {
-				std::string skip_name = "Skip_FC" + std::to_string(i);
-				model_builder.addSinglyConnected(model, skip_name, node_names_input, node_names,
-					std::shared_ptr<WeightInitOp<TensorT>>(new RandWeightInitOp<TensorT>(n_inputs, 2)),
-					std::shared_ptr<SolverOp<TensorT>>(new AdamOp<TensorT>(0.001, 0.9, 0.999, 1e-8)), 0.0f);
-			}
+			//if (add_skip) {
+			//	std::string skip_name = "Skip_FC" + std::to_string(i);
+			//	model_builder.addSinglyConnected(model, skip_name, node_names_input, node_names,
+			//		std::shared_ptr<WeightInitOp<TensorT>>(new RandWeightInitOp<TensorT>(n_inputs, 2)),
+			//		std::shared_ptr<SolverOp<TensorT>>(new AdamOp<TensorT>(0.001, 0.9, 0.999, 1e-8)), 0.0f);
+			//}
 			node_names_input = node_names;
 		}
 
@@ -208,7 +208,7 @@ public:
 					for (int nodes_iter = 0; nodes_iter < this->validation_data.dimension(1); ++nodes_iter) {
 						input_data(batch_iter, memory_iter, nodes_iter, epochs_iter) = this->validation_data(sample_indices[epochs_iter*batch_size + batch_iter], nodes_iter);
 					}
-	  		  for (int nodes_iter = 0; nodes_iter < this->validation_labels.dimension(1); ++nodes_iter) {
+					for (int nodes_iter = 0; nodes_iter < this->validation_labels.dimension(1); ++nodes_iter) {
 						output_data(batch_iter, memory_iter, nodes_iter, epochs_iter) = (TensorT)this->validation_labels(sample_indices[epochs_iter*batch_size + batch_iter], nodes_iter);
 						//output_data(batch_iter, memory_iter, nodes_iter + this->validation_labels.dimension(1), epochs_iter) = (TensorT)this->validation_labels(sample_indices[epochs_iter*batch_size + batch_iter], nodes_iter);
 					}
@@ -303,19 +303,11 @@ void main_DotProdAttention() {
 	// read in the training data
 	const std::string training_data_filename = "C:/Users/domccl/GitHub/mnist/train-images.idx3-ubyte";
 	const std::string training_labels_filename = "C:/Users/domccl/GitHub/mnist/train-labels.idx1-ubyte";
-	//const std::string training_data_filename = "C:/Users/dmccloskey/Documents/GitHub/mnist/train-images-idx3-ubyte";
-	//const std::string training_labels_filename = "C:/Users/dmccloskey/Documents/GitHub/mnist/train-labels-idx1-ubyte";
-	//const std::string training_data_filename = "/home/user/data/train-images-idx3-ubyte";
-	//const std::string training_labels_filename = "/home/user/data/train-labels-idx1-ubyte";
 	data_simulator.readData(training_data_filename, training_labels_filename, true, training_data_size, input_size);
 
 	// read in the validation data
 	const std::string validation_data_filename = "C:/Users/domccl/GitHub/mnist/t10k-images.idx3-ubyte";
 	const std::string validation_labels_filename = "C:/Users/domccl/GitHub/mnist/t10k-labels.idx1-ubyte";
-	//const std::string validation_data_filename = "C:/Users/dmccloskey/Documents/GitHub/mnist/t10k-images-idx3-ubyte";
-	//const std::string validation_labels_filename = "C:/Users/dmccloskey/Documents/GitHub/mnist/t10k-labels-idx1-ubyte";
-	//const std::string validation_data_filename = "/home/user/data/t10k-images-idx3-ubyte";
-	//const std::string validation_labels_filename = "/home/user/data/t10k-labels-idx1-ubyte";
 	data_simulator.readData(validation_data_filename, validation_labels_filename, false, validation_data_size, input_size);
 	data_simulator.unitScaleData();
 
@@ -325,9 +317,9 @@ void main_DotProdAttention() {
 		input_nodes.push_back("Input_" + std::to_string(i));
 
 	// Make the output nodes
-	std::vector<std::string> output_FC_nodes;
+	std::vector<std::string> output_nodes;
 	for (int i = 0; i < data_simulator.mnist_labels.size(); ++i)
-		output_FC_nodes.push_back("Output_" + std::to_string(i));
+		output_nodes.push_back("Output_" + std::to_string(i));
 
 	// define the model trainers and resources for the trainers
 	std::vector<ModelInterpreterGpu<float>> model_interpreters;
@@ -351,7 +343,7 @@ void main_DotProdAttention() {
 		//std::shared_ptr<LossFunctionGradOp<float>>({new MSEGradOp<float>())//,	
 		std::shared_ptr<LossFunctionGradOp<float>>(new CrossEntropyWithLogitsGradOp<float>())
 		});
-	model_trainer.setOutputNodes({ output_FC_nodes
+	model_trainer.setOutputNodes({ output_nodes
 		});
 
 	// define the model replicator for growth mode
@@ -360,7 +352,7 @@ void main_DotProdAttention() {
 	// define the initial population
 	std::cout << "Initializing the population..." << std::endl;
 	std::vector<Model<float>> population = { ModelTrainerExt<float>().makeMultiHeadDotProdAttention(input_nodes.size(), output_nodes.size(), 8, 2, true, false) };
-	
+
 
 	// Evolve the population
 	std::vector<std::vector<std::tuple<int, std::string, float>>> models_validation_errors_per_generation = population_trainer.evolveModels(
@@ -376,5 +368,5 @@ int main(int argc, char** argv)
 	// run the application
 	main_DotProdAttention();
 
-  return 0;
+	return 0;
 }
