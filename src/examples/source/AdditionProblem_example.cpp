0ef04ede012030ddd8ad5cb6fe97c1a64d4c85fd
dmccloskey <dmccloskey87@gmail.com>
2019-01-11 13:49:47 -0800

    fix: gcc build errors

--- src/examples/source/AdditionProblem_example.cpp
+++ src/examples/source/AdditionProblem_example.cpp
@@ -33,7 +33,7 @@ public:
 		//// TODO: ensure that the this->sequence_length_ >= memory_size!
 		//Eigen::Tensor<TensorT, 1> random_sequence(this->sequence_length_);
 		//Eigen::Tensor<TensorT, 1> mask_sequence(this->sequence_length_);
-		//float result = AddProb(random_sequence, mask_sequence, this->n_mask_);
+		//float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
 
 		// Generate the input and output data for training [BUG FREE]
 		for (int batch_iter = 0; batch_iter<batch_size; ++batch_iter) {
@@ -43,7 +43,7 @@ public:
 				// TODO: ensure that the this->sequence_length_ >= memory_size!
 				Eigen::Tensor<float, 1> random_sequence(this->sequence_length_);
 				Eigen::Tensor<float, 1> mask_sequence(this->sequence_length_);
-				float result = AddProb(random_sequence, mask_sequence, this->n_mask_);
+				float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
 				Eigen::Tensor<float, 1> cumulative(this->sequence_length_);
 				cumulative.setZero();
 
