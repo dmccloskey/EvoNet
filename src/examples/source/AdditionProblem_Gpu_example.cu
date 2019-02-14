7bbcf17cbcd7e039ae4ba32d30b86d2da2eb7aff
Douglas McCloskey <dmccloskey87@gmail.com>
2019-02-14 16:04:51 +0100

    fix: replaced all asserts with throw in modelInterpreter

--- src/examples/source/AdditionProblem_Gpu_example.cu
+++ src/examples/source/AdditionProblem_Gpu_example.cu
@@ -29,21 +29,21 @@ public:
 		const int n_output_nodes = output_data.dimension(2);
 		const int n_epochs = input_data.dimension(3);
 
-		// generate a new sequence 
-		// TODO: ensure that the this->sequence_length_ >= memory_size!
-		Eigen::Tensor<TensorT, 1> random_sequence(this->sequence_length_);
-		Eigen::Tensor<TensorT, 1> mask_sequence(this->sequence_length_);
-		float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
+		//// generate a new sequence 
+		//// TODO: ensure that the this->sequence_length_ >= memory_size!
+		//Eigen::Tensor<TensorT, 1> random_sequence(this->sequence_length_);
+		//Eigen::Tensor<TensorT, 1> mask_sequence(this->sequence_length_);
+		//float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
 
 		// Generate the input and output data for training [BUG FREE]
 		for (int batch_iter = 0; batch_iter < batch_size; ++batch_iter) {
 			for (int epochs_iter = 0; epochs_iter < n_epochs; ++epochs_iter) {
 
-				//// generate a new sequence 
-				//// TODO: ensure that the this->sequence_length_ >= memory_size!
-				//Eigen::Tensor<float, 1> random_sequence(this->sequence_length_);
-				//Eigen::Tensor<float, 1> mask_sequence(this->sequence_length_);
-				//float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
+				// generate a new sequence 
+				// TODO: ensure that the this->sequence_length_ >= memory_size!
+				Eigen::Tensor<float, 1> random_sequence(this->sequence_length_);
+				Eigen::Tensor<float, 1> mask_sequence(this->sequence_length_);
+				float result = this->AddProb(random_sequence, mask_sequence, this->n_mask_);
 				Eigen::Tensor<float, 1> cumulative(this->sequence_length_);
 				cumulative.setZero();
 
