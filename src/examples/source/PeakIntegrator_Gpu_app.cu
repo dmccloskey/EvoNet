f0cb35b96d4cc1e2993febd69b4176483a91251f
Douglas McCloskey <dmccloskey87@gmail.com>
2019-03-14 12:39:34 +0100

    fix: small change to peak integrator app

--- src/examples/source/PeakIntegrator_Gpu_app.cu
+++ src/examples/source/PeakIntegrator_Gpu_app.cu
@@ -786,8 +786,8 @@ void main_DenoisingAE(const bool& make_model, const bool& train_model) {
   model_trainer.setBatchSize(1); // evaluation only
   //model_trainer.setBatchSize(64);
   //model_trainer.setNEpochsTraining(5000);
-  model_trainer.setNEpochsTraining(10);
-  model_trainer.setNEpochsValidation(10);
+  model_trainer.setNEpochsTraining(0);
+  model_trainer.setNEpochsValidation(25);
   model_trainer.setNEpochsEvaluation(10);
   model_trainer.setMemorySize(1);
   model_trainer.setVerbosityLevel(1);
