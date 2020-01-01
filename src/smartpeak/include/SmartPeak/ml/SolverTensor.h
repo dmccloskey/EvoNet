edcf826299922521ff5c5b154c5f6c3fb0ab785f
dmccloskey <dmccloskey87@gmail.com>
2020-01-01 12:29:39 -0800

    feat, tests: adding SVAGTensorOp

--- src/smartpeak/include/SmartPeak/ml/SolverTensor.h
+++ src/smartpeak/include/SmartPeak/ml/SolverTensor.h
@@ -44,20 +44,18 @@ public:
     TensorT getGradientNoiseSigma() const{return gradient_noise_sigma_;};
     void setGradientNoiseGamma(const TensorT& gradient_noise_gamma){gradient_noise_gamma_ = gradient_noise_gamma;};
     TensorT getGradientNoiseGamma() const{return gradient_noise_gamma_;};
+    void setEps(const TensorT& eps) { eps_ = eps; };
+    TensorT getEps() const { return eps_; }
     //virtual std::string getParameters() const = 0;
     TensorT annealGradientNoiseSigma(const TensorT& iter) {
       const TensorT sigma_annealed = gradient_noise_sigma_ / std::pow((1 + iter), gradient_noise_gamma_); // annealed variance
       return sigma_annealed;
     }
 	private:
-    // clipping parameters
-    TensorT gradient_threshold_ = TensorT(1e6); ///< maximum gradient magnitude
+    TensorT gradient_threshold_ = TensorT(1e6); ///< maximum gradient magnitude for gradient clipping
     TensorT learning_rate_ = TensorT(1e-3); ///< the learning rate
-
-    // gradient noise with annealed variance parameters
-    TensorT gradient_noise_sigma_ = TensorT(0.0); ///< variance before annealing (0.0 = none, 1.0 = normal distribution with mean = 0 and var = 1.0)
-    TensorT gradient_noise_gamma_ = TensorT(0.55); ///< iter-dependend annealing factor
-    
+    TensorT gradient_noise_sigma_ = TensorT(0.0); ///< variance before annealing (0.0 = none, 1.0 = normal distribution with mean = 0 and var = 1.0) for gradient noise
+    TensorT gradient_noise_gamma_ = TensorT(0.55); ///< iter-dependend annealing factor for gradient noise    
     TensorT eps_ = TensorT(1e-24);
   };
 
@@ -95,9 +93,10 @@ public:
       auto noise = weights_tensor.random()*weights_tensor.constant(this->annealGradientNoiseSigma(iter + 1));
       auto errors_noise = errors_clipped + noise;
 
-      // Weight updates (omitting the bias correction step)
-			solver_params_tensor.chip(2, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(2,2) + (errors_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * errors_clipped;
-			weights_tensor.device(device) -= solver_params_tensor.chip(0, 2) * solver_params_tensor.chip(2, 2);
+      // Weight updates
+			solver_params_tensor.chip(2, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(2,2) + (errors_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * errors_noise;
+			auto velocity_unbiased = solver_params_tensor.chip(2, 2) / (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2).pow(iter + 1));
+      weights_tensor.device(device) -= solver_params_tensor.chip(0, 2) * velocity_unbiased;
     };
     void setMomentum(const TensorT& momentum) { momentum_ = momentum; };
     TensorT getMomentum() const { return momentum_; };
@@ -191,10 +190,10 @@ public:
       auto errors_noise = errors_clipped + noise;
 
       // Weight updates (omitting the bias correction step)
-			solver_params_tensor.chip(4, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(4, 2) + (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2)) * errors_clipped;
-			solver_params_tensor.chip(5, 2).device(device) = solver_params_tensor.chip(2, 2) * solver_params_tensor.chip(5, 2) + (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(2, 2)) * errors_clipped.pow(2);
-      //auto unbiased_adam1 = solver_params_tensor.chip(4, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2));
-      //auto unbiased_adam2 = solver_params_tensor.chip(5, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(2, 2));
+			solver_params_tensor.chip(4, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(4, 2) + (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * errors_noise;
+			solver_params_tensor.chip(5, 2).device(device) = solver_params_tensor.chip(2, 2) * solver_params_tensor.chip(5, 2) + (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(2, 2)) * errors_noise.pow(2);
+      auto unbiased_adam1 = solver_params_tensor.chip(4, 2) / (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2).pow(iter + 1));
+      auto unbiased_adam2 = solver_params_tensor.chip(5, 2) / (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(2, 2).pow(iter + 1));
 			weights_tensor.device(device) -= solver_params_tensor.chip(0, 2) * solver_params_tensor.chip(4, 2) / (solver_params_tensor.chip(5, 2).sqrt() + solver_params_tensor.chip(3, 2));
     };
     void setMomentum(const TensorT& momentum) { momentum_ = momentum; };
@@ -205,9 +204,9 @@ public:
     TensorT getDelta() const { return delta_; };
     std::string getName() const{return "AdamTensorOp";};
 	private:
-    TensorT momentum_ = (TensorT)0.9; ///< Momentum
-    TensorT momentum2_ = (TensorT)0.999; ///< Momentum2
-    TensorT delta_ = (TensorT)1e-8; ///< Delta
+    TensorT momentum_ = TensorT(0.9); ///< Momentum
+    TensorT momentum2_ = TensorT(0.999); ///< Momentum2
+    TensorT delta_ = TensorT(1e-8); ///< Delta
   };
 
   /**
@@ -227,7 +226,7 @@ public:
 
     @params weights Data for the weight tensor
     @params errorr Data for the weight tensor errors
-		@params solver_params Data for the solver params (Dim 2, size 5: learning rate, momentum, momentum_prev, variance_prev, beta_power)
+		@params solver_params Data for the solver params (Dim 2, size 4: learning rate, momentum, momentum_prev, variance_prev)
     @param source_layer_size Dim 0
     @param sink_layer_size Dim 1
     */
@@ -235,7 +234,7 @@ public:
     {
       Eigen::TensorMap<Eigen::Tensor<TensorT, 2>> weights_tensor(weights, source_layer_size, sink_layer_size);
       Eigen::TensorMap<Eigen::Tensor<TensorT, 2>> errors_tensor(errors, source_layer_size, sink_layer_size);
-      Eigen::TensorMap<Eigen::Tensor<TensorT, 3>> solver_params_tensor(solver_params, source_layer_size, sink_layer_size, 6);
+      Eigen::TensorMap<Eigen::Tensor<TensorT, 3>> solver_params_tensor(solver_params, source_layer_size, sink_layer_size, 4);
 
       // Remove Nans
       auto errors_no_nans = (errors_tensor == errors_tensor).select(errors_tensor, errors_tensor.constant(TensorT(0)));
@@ -250,25 +249,22 @@ public:
 
       // Calculate Rho
       auto rho = (
-        (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)).pow(0.5) * (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(4, 2).pow(0.5))) / (
-          (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2).pow(0.5)) * (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(4, 2)).pow(0.5)
-          ).cwiseMin(weights_tensor.constant(TensorT(1) - this->eps_));
+        (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * (weights_tensor.constant(TensorT(1)) + solver_params_tensor.chip(1, 2).pow(iter + 1))) / (
+          (weights_tensor.constant(TensorT(1)) + solver_params_tensor.chip(1, 2)) * (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)).pow(iter + 1)
+          ).cwiseMin(weights_tensor.constant(TensorT(1) - this->getEps()));
 
       // Calculate momentum and variance estimates
-      //mt_update = mt.assign(self._beta_t*mt + (1.0 - self._beta_t)*grad,
-      //  use_locking = self._use_locking)
-      solver_params_tensor.chip(2, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(2, 2) + (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2)) * weights_tensor * errors_clipped;
-        //vt_update = vt.assign(self._beta_t*vt + (1.0 - self._beta_t)*tf.square(grad),
-        //  use_locking = self._use_locking)
+      solver_params_tensor.chip(2, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(2, 2) + (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * errors_noise;
+      solver_params_tensor.chip(3, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(3, 2) + (weights_tensor.constant(TensorT(1)) - solver_params_tensor.chip(1, 2)) * errors_noise.pow(2);
+      auto unbiased_mean = solver_params_tensor.chip(2, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2).pow(iter + 1));
+      auto unbiased_var = solver_params_tensor.chip(3, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2).pow(iter + 1));
+      auto var_estimate = (unbiased_var - unbiased_mean.pow(2)) / (weights_tensor.constant((TensorT)1) - rho);
+      auto gamma = unbiased_mean.pow(2) / (unbiased_mean.pow(2) + rho * var_estimate);
 
       // Weight updates
-      solver_params_tensor.chip(4, 2).device(device) = solver_params_tensor.chip(1, 2) * solver_params_tensor.chip(4, 2) + (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2)) * weights_tensor * errors_clipped;
-      solver_params_tensor.chip(5, 2).device(device) = solver_params_tensor.chip(2, 2) * solver_params_tensor.chip(5, 2) + (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(2, 2)) * weights_tensor * errors_clipped * weights_tensor * errors_clipped;
-      auto unbiased_adam1 = solver_params_tensor.chip(4, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(1, 2));
-      auto unbiased_adam2 = solver_params_tensor.chip(5, 2) / (weights_tensor.constant((TensorT)1) - solver_params_tensor.chip(2, 2));
-      weights_tensor.device(device) -= solver_params_tensor.chip(0, 2) * unbiased_adam1 / (unbiased_adam2.sqrt() + solver_params_tensor.chip(3, 2));
+      weights_tensor.device(device) -= solver_params_tensor.chip(0, 2) * gamma * unbiased_mean;
     };
-    std::string getName() const { return "AdamTensorOp"; };
+    std::string getName() const { return "SVAGTensorOp"; };
   };
 
 	/**
--- src/tests/class_tests/smartpeak/source/SolverTensor_test.cpp
+++ src/tests/class_tests/smartpeak/source/SolverTensor_test.cpp
@@ -32,6 +32,7 @@ BOOST_AUTO_TEST_CASE(settersAndGetters)
   BOOST_CHECK_CLOSE(operation.getGradientThreshold(), 10.0, 1e4);
   BOOST_CHECK_CLOSE(operation.getGradientNoiseSigma(), 1.0, 1e4);
   BOOST_CHECK_CLOSE(operation.getGradientNoiseGamma(), 0.55, 1e4);
+  BOOST_CHECK_CLOSE(operation.getEps(), 1e-24, 1e4);
   //BOOST_CHECK_EQUAL(operation.getParameters(), "gradient_threshold:1000000.000000;gradient_noise_sigma:1.000000;gradient_noise_gamma:0.550000;learning_rate:0.900000;momentum:0.100000;momentum_prev:0.000000");
 
   SSDTensorOp<float, Eigen::DefaultDevice> ssd_op(10.0f, 1.0f, 0.55f);
@@ -39,6 +40,7 @@ BOOST_AUTO_TEST_CASE(settersAndGetters)
   BOOST_CHECK_CLOSE(ssd_op.getGradientThreshold(), 10.0, 1e4);
   BOOST_CHECK_CLOSE(ssd_op.getGradientNoiseSigma(), 1.0, 1e4);
   BOOST_CHECK_CLOSE(ssd_op.getGradientNoiseGamma(), 0.55, 1e4);
+  BOOST_CHECK_CLOSE(ssd_op.getEps(), 1e-24, 1e4);
   //BOOST_CHECK_EQUAL(ssd_op.getParameters(), "gradient_threshold:1000000.000000;gradient_noise_sigma:1.000000;gradient_noise_gamma:0.550000;learning_rate:0.900000;momentum:0.100000;momentum_prev:0.000000");
 
   AdamTensorOp<float, Eigen::DefaultDevice> adam_op(10.0f, 1.0f, 0.55f);
@@ -46,21 +48,31 @@ BOOST_AUTO_TEST_CASE(settersAndGetters)
   BOOST_CHECK_CLOSE(adam_op.getGradientThreshold(), 10.0, 1e4);
   BOOST_CHECK_CLOSE(adam_op.getGradientNoiseSigma(), 1.0, 1e4);
   BOOST_CHECK_CLOSE(adam_op.getGradientNoiseGamma(), 0.55, 1e4);
+  BOOST_CHECK_CLOSE(adam_op.getEps(), 1e-24, 1e4);
   //BOOST_CHECK_EQUAL(adam_op.getParameters(), "gradient_threshold:1000000.000000;gradient_noise_sigma:1.000000;gradient_noise_gamma:0.550000;learning_rate:0.010000;momentum:0.900000;momentum2:0.999000;delta:0.000000;momentum_prev:0.000000;momentum2_prev:0.000000");
+
+  SVAGTensorOp<float, Eigen::DefaultDevice> svag_op(10.0f, 1.0f, 0.55f);
+  BOOST_CHECK_EQUAL(svag_op.getName(), "SVAGTensorOp");
+  BOOST_CHECK_CLOSE(svag_op.getGradientThreshold(), 10.0, 1e4);
+  BOOST_CHECK_CLOSE(svag_op.getGradientNoiseSigma(), 1.0, 1e4);
+  BOOST_CHECK_CLOSE(svag_op.getGradientNoiseGamma(), 0.55, 1e4);
+  BOOST_CHECK_CLOSE(svag_op.getEps(), 1e-24, 1e4);
+  //BOOST_CHECK_EQUAL(svag_op.getParameters(), "gradient_threshold:1000000.000000;gradient_noise_sigma:1.000000;gradient_noise_gamma:0.550000;learning_rate:0.010000;momentum:0.900000;momentum2:0.999000;delta:0.000000;momentum_prev:0.000000;momentum2_prev:0.000000");
 
 	DummySolverTensorOp<float, Eigen::DefaultDevice> dummy_solver_op(10.0f, 1.0f, 0.55f);
 	BOOST_CHECK_EQUAL(dummy_solver_op.getName(), "DummySolverTensorOp");
   BOOST_CHECK_CLOSE(dummy_solver_op.getGradientThreshold(), 10.0, 1e4);
   BOOST_CHECK_CLOSE(dummy_solver_op.getGradientNoiseSigma(), 1.0, 1e4);
   BOOST_CHECK_CLOSE(dummy_solver_op.getGradientNoiseGamma(), 0.55, 1e4);
+  BOOST_CHECK_CLOSE(dummy_solver_op.getEps(), 1e-24, 1e4);
 	//BOOST_CHECK_EQUAL(dummy_solver_op.getParameters(), "");
 }
 
 BOOST_AUTO_TEST_CASE(annealGradientNoiseSigma)
 {
   SGDTensorOp<float, Eigen::DefaultDevice> operation(10.0f, 1.0f, 0.55f);
-  BOOST_CHECK_CLOSE(operation.annealGradientNoiseSigma(1), 0, 1e-4);
-  BOOST_CHECK_CLOSE(operation.annealGradientNoiseSigma(100), 0, 1e-4);
+  BOOST_CHECK_CLOSE(operation.annealGradientNoiseSigma(1), 0.683020115, 1e-4);
+  BOOST_CHECK_CLOSE(operation.annealGradientNoiseSigma(100), 0.0789992958, 1e-4);
 }
 
 BOOST_AUTO_TEST_CASE(operationfunctionSGDOp) 
@@ -287,6 +299,82 @@ BOOST_AUTO_TEST_CASE(operationfunctionAdamOp)
   BOOST_CHECK_CLOSE(solver_params(1, 0, 3), 1e-8, 1e-4);
   BOOST_CHECK(solver_params(1, 0, 4) != 1.00000024, 1e-4);
   BOOST_CHECK(solver_params(1, 0, 5) != 0.0999987125, 1e-4);
+}
+
+BOOST_AUTO_TEST_CASE(operationfunctionSVAGOp)
+{
+  AdamTensorOp<float, Eigen::DefaultDevice> operation;
+
+  const int sink_layer_size = 1;
+  const int source_layer_size = 2;
+  const int iter = 0;
+  Eigen::Tensor<float, 2> weights(source_layer_size, sink_layer_size);
+  weights.setConstant(1);
+  Eigen::Tensor<float, 2> errors(source_layer_size, sink_layer_size);
+  errors.setValues({ {0.1},	{10} });
+  Eigen::Tensor<float, 3> solver_params(source_layer_size, sink_layer_size, 4);
+  solver_params.setValues({ {{0.01, 0.9, 0.0, 0.0}},
+    {{0.01, 0.9, 0.0, 0.0}} });
+
+  Eigen::DefaultDevice device;
+
+  // Test operator call
+  operation(weights.data(), errors.data(), solver_params.data(), source_layer_size, sink_layer_size, iter, device);
+  BOOST_CHECK_CLOSE(weights(0, 0), 0.968377113, 1e-4);
+  BOOST_CHECK_CLOSE(weights(1, 0), 0.968377113, 1e-4);
+  BOOST_CHECK_CLOSE(errors(0, 0), 0.1, 1e-4);
+  BOOST_CHECK_CLOSE(errors(1, 0), 10.0, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 2), 0.0100000026, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 3), 9.99987151e-06, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 2), 1.00000024, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 3), 0.0999987125, 1e-4);
+
+  // Test second operator call
+  Eigen::Tensor<float, 2> weights1(source_layer_size, sink_layer_size);
+  weights1.setConstant(1);
+  Eigen::Tensor<float, 3> solver_params1(source_layer_size, sink_layer_size, 4);
+  solver_params1.setValues({ {{0.01, 0.9, 0.0, 0.0}},
+    {{0.01, 0.9, 0.0, 0.0}} });
+  operation.setGradientThreshold(1.0);
+
+  operation(weights1.data(), errors.data(), solver_params1.data(), source_layer_size, sink_layer_size, iter, device);
+  BOOST_CHECK_CLOSE(weights1(0, 0), 0.968377113, 1e-4);
+  BOOST_CHECK_CLOSE(weights1(1, 0), 0.968377113, 1e-4);
+  BOOST_CHECK_CLOSE(errors(0, 0), 0.1, 1e-4);
+  BOOST_CHECK_CLOSE(errors(1, 0), 10.0, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(0, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(0, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(0, 0, 2), 0.0100000026, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(0, 0, 3), 9.99987151e-06, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(1, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(1, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(1, 0, 2), 0.1000, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params1(1, 0, 3), 0.000999987125, 1e-4);
+
+  // Test operator call with noise
+  operation.setGradientNoiseSigma(10.0f);
+  weights.setConstant(1);
+  errors.setValues({ {0.1},	{10} });
+  solver_params.setValues({ {{0.01, 0.9, 0.0, 0.0}},
+    {{0.01, 0.9, 0.0, 0.0}} });
+
+  operation(weights.data(), errors.data(), solver_params.data(), source_layer_size, sink_layer_size, iter, device);
+  BOOST_CHECK(weights(0, 0) != 0.968377113, 1e-4);
+  BOOST_CHECK(weights(1, 0) != 0.968377113, 1e-4);
+  BOOST_CHECK_CLOSE(errors(0, 0), 0.1, 1e-4);
+  BOOST_CHECK_CLOSE(errors(1, 0), 10.0, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 2), 0.0100000026, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(0, 0, 3), 9.99987151e-06, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 0), 0.01, 1e-4);
+  BOOST_CHECK_CLOSE(solver_params(1, 0, 1), 0.9, 1e-4);
+  BOOST_CHECK(solver_params(1, 0, 2) != 1.00000024, 1e-4);
+  BOOST_CHECK(solver_params(1, 0, 3) != 0.0999987125, 1e-4);
 }
 
 BOOST_AUTO_TEST_CASE(operationfunctionDummySolverOp)
