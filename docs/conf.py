b286f93017fb2a354fd96a4cff7d12b49d5300e0
dmccloskey <dmccloskey87@gmail.com>
2022-02-13 16:23:34 +0100

    fix: debugging doxygen 5

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ exhale_args = {
     "createTreeView":        True,
     # TIP: if using the sphinx-bootstrap-theme, you need
     # "treeViewIsBootstrap": True,
-    #"exhaleExecutesDoxygen": True,
+    "exhaleExecutesDoxygen": False, #True,
     "exhaleDoxygenStdin":    "INPUT = ../src/evonet/include/EvoNet"
 }
 
